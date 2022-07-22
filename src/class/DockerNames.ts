bb2efd608333d75f5a0765dc69288e155f5e0a1e
Mohammadreza <mrvashian@gmail.com>
2022-07-22 22:03:28 +0430

    make ready generate random name

---
+++ src/class/DockerNames.ts
@@ -0,0 +1,48 @@
+import { customAlphabet } from 'nanoid';
+
+import { leftNames, rightNames } from '../data/names';
+
+class DockerNames {
+  private adjectives: string[];
+  private surnames: string[];
+
+  constructor() {
+    this.adjectives = leftNames;
+    this.surnames = rightNames;
+  }
+
+  public getRandomName(randChar?: boolean | number): string {
+    const randomName = this.generateRandomName(this.adjectives, this.surnames);
+
+    if (randChar === true || randChar > 0) {
+      const length: number = typeof randChar === 'number' ? randChar : 6;
+
+      return `${randomName}_${this.getRandomChar(length)}`;
+    }
+
+    return randomName;
+  }
+
+  private generateRandomName(
+    leftNames: string[],
+    rightNames: string[],
+  ): string {
+    const left = leftNames[Math.floor(Math.random() * leftNames.length)];
+    const right = rightNames[Math.floor(Math.random() * rightNames.length)];
+
+    const name = `${left}_${right}`;
+
+    /* Steve Wozniak is not boring. This is part of the docker names spec. */
+    if (name === 'boring_wozniak') {
+      return this.generateRandomName(leftNames, rightNames);
+    }
+
+    return name;
+  }
+
+  private getRandomChar(length: number): string {
+    return customAlphabet('1234567890abcdefghiklmnopqrstuv', length)();
+  }
+}
+
+export default DockerNames;
